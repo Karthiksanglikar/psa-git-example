7da56412a9339b33c2f11b92fdde85dc892a8973
Karthiksanglikar <156422000+Karthiksanglikar@users.noreply.github.com>
2025-03-13 11:49:52 +0530

    Revert "Feature"

--- src/main/java/com/psa/PsaGitExampleApplication.java
+++ src/main/java/com/psa/PsaGitExampleApplication.java
@@ -9,7 +9,6 @@ public class PsaGitExampleApplication {
 	public static void main(String[] args) {
 		int x=100;
 		int y=10;
-		char c='a';
 		String z="hello";
 		SpringApplication.run(PsaGitExampleApplication.class, args);
 	}

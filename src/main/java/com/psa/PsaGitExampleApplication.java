88d7be3ee9aa6da19d38e4bcc137c9d324de0403
Karthiksanglikar <156422000+Karthiksanglikar@users.noreply.github.com>
2025-03-13 11:33:45 +0530

    Update PsaGitExampleApplication.java

--- src/main/java/com/psa/PsaGitExampleApplication.java
+++ src/main/java/com/psa/PsaGitExampleApplication.java
@@ -8,13 +8,9 @@ public class PsaGitExampleApplication {
 
 	public static void main(String[] args) {
 		int x=100;
-<<<<<<< HEAD
-		boolean ispresent=false;
-=======
 		int y=10;
 		String z="hello";
 		Double d=10.3;
->>>>>>> c5b29168a352e51cd9912a1ae00344cf48ef9f31
 		SpringApplication.run(PsaGitExampleApplication.class, args);
 	}
 
